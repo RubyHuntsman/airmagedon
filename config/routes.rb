37908124641aa7c11fb517963fade75414be30f7
Hubert Kuczmierczyk <PublicProgramer@gmx.com>
2018-10-12 12:35:23 +0200

    Add login by login with devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'dashboard/index'
+  root 'dashboard#index'
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :installations
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
