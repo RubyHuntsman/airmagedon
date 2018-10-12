b604ea24da25a94ec85fcd9f84163de1e1abd209
Hubert Kuczmierczyk <PublicProgramer@gmx.com>
2018-10-12 13:08:34 +0200

    Add authorization to admin panel

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
-    params.require(:user).permit(:login, :password, :password_confirmation)
+    params.require(:user).permit(:login, :password, :password_confirmation, :email)
   end
  
   def account_update_params
-    params.require(:user).permit(:login, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:login, :password, :password_confirmation, :current_password, :email)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_12_100900) do
+ActiveRecord::Schema.define(version: 2018_10_12_110010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2018_10_12_100900) do
 
   create_table "users", force: :cascade do |t|
     t.string "login", default: "", null: false
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.boolean "is_admin", default: false, null: false
     t.string "reset_password_token"
@@ -43,7 +42,7 @@ ActiveRecord::Schema.define(version: 2018_10_12_100900) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string "email", default: "", null: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
