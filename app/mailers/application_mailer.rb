71fee5197b9a80f4886536801810d616e11bad7d
Hubert Kuczmierczyk <PublicProgramer@gmx.com>
2018-10-18 12:32:51 +0200

    Working email sending

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'maciej.lokio@interia.pl'
   layout 'mailer'
 
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,7 +14,7 @@ class UserMailer < ApplicationMailer
       live.push(m.last) if m.last.created_at > 3.hours.ago
     end
     @om = live.select { |m| m.qi <= user.critical }
-    followed = user.subscriptions.ids
+    followed = user.subscriptions.map { |s| s.installation_id }
     @om = @om.select { |m| followed.include?(m.installation_id)}
 
 		mail(to: user.email,
