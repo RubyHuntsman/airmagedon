4231bcbd66626bf5ea0a39c06083dda5f749fb69
Klawus <klawy78@gmail.com>
2018-10-20 23:25:41 +0200

    some bugfix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,9 +4,12 @@ class DashboardController < ApplicationController
   def index
     @installations_count = Installation.count
 
+    @Measurement_last = 0
+
 		live = []
     Measurement.last(2000).group_by{ |s| s.installation_id }.each do |s, m|
       live.push(m.last) if m.last.created_at > 3.hours.ago
+      @Measurement_last = m.last.measured_at.in_time_zone("Warsaw").strftime("%H:%M")
     end
     @live = live.sort_by{|m| -m[:pm10]}
 
